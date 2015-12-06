403e15de0a1db34f41a50f1c31e9e9bac3394943
Bryan Nissen <bryan@webizly.com>
2015-12-06 05:37:03 -0500

    Initial Yeoman Scaffold

---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('projectTimesheetApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
